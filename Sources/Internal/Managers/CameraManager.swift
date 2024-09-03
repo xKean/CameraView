f4d4c0ae7c9c144315bcaf12287326d595759084
Peter Braun <peter.braun99@gmx.net>
2024-09-03 18:15:30 +0200

    changed camera res

--- Sources/Internal/Managers/CameraManager.swift
+++ Sources/Internal/Managers/CameraManager.swift
@@ -28,7 +28,7 @@ public class CameraManager: NSObject, ObservableObject { init(_ attributes: Attr
         var torchMode: CameraTorchMode = .off
         var cameraExposure: CameraExposure = .init()
         var hdrMode: CameraHDRMode = .auto
-        var resolution: AVCaptureSession.Preset = .hd1920x1080
+        var resolution: AVCaptureSession.Preset = .high
         var frameRate: Int32 = 30
         var mirrorOutput: Bool = false
         var isGridVisible: Bool = true
